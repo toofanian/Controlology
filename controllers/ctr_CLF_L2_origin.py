8179352ac2ffcd44895bccf6ee50c71e5a1f1ede
Alex Toofanian <alextoofanian@gmail.com>
2022-05-30 01:11:32 -0700

    integrated clfqp utility function

--- controllers/ctr_CLF_L2_origin.py
+++ controllers/ctr_CLF_L2_origin.py
@@ -4,11 +4,13 @@ import numpy as np
 import cvxpy as cp
 from typing import Tuple,Optional,Union
 
-class Controller_CLF2Orig(Controller): 
+from utilities.clfqp import makeproblem_clfqp
+
+class Controller_CLF_L2_Origin(Controller): 
     def __init__(self, sys:ControlAffineSys) -> None:
         super().__init__(sys=sys)
 
-        self.problem = self._makeproblem_clfqp()
+        self.problem = makeproblem_clfqp(self)
     
     def u(self, t:float, x:np.ndarray) -> np.ndarray:
         self.u_ref_param.value = np.array([[0.]])
@@ -28,34 +30,3 @@ class Controller_CLF2Orig(Controller):
     def clf(self, x:np.ndarray) -> Tuple[float,np.ndarray]:
         return np.array([[np.linalg.norm(x)**2]]) / 2 , x.T
     
-    def _makeproblem_clfqp(self) -> cp.Problem:
-        '''
-        sets up the CLFQP
-        '''
-        ### define variables and parameters
-        self.u_var = cp.Variable((self.sys.uDims,1))
-        self.u_ref_param = cp.Parameter((self.sys.uDims,1))
-        self.r_var = cp.Variable((1,1),nonneg=True)
-        self.r_penalty_param = cp.Parameter((1,1),nonneg=True)
-        self.V_param = cp.Parameter((1,1),nonneg=True) 
-        self.LfV_param =  cp.Parameter((1,1))
-        self.LgV_param = cp.Parameter((1,self.sys.uDims))
-        c = 1 # hyperparameter, should be tied to one used during training
-
-        ### define objective
-        objective_expression = cp.sum_squares(self.u_var - self.u_ref_param) + cp.multiply(self.r_var,self.r_penalty_param)
-        objective = cp.Minimize(objective_expression)
-
-        ### define constraints
-        constraints = []
-        # control constraints
-        for iu in range(self.sys.uDims):
-            constraints.append(self.u_var[iu] >= self.sys.uBounds[iu,0])
-            constraints.append(self.u_var[iu] <= self.sys.uBounds[iu,1])
-        # CLF constraint
-        constraints.append(self.LfV_param + self.LgV_param@self.u_var <= -self.V_param + self.r_var)
-
-        ### assemble problem
-        problem = cp.Problem(objective,constraints)
-        assert problem.is_qp(), 'Problem is not qp'
-        return problem
