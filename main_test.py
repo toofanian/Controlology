db3e6b7de6817d4bbfee211d1755e9d02a87df8b
Alex Toofanian <alextoofanian@gmail.com>
2022-05-30 01:13:08 -0700

    cleanup

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ import numpy as np
 ###############################################################
 
 from systems.sys_SingleIntegrator           import Sys_SingleIntegrator
-from controllers.ctr_nCLF                   import Ctr_nCLF
+from controllers.ctr_CLF_L2_Origin          import Controller_CLF_L2_Origin
 from simulators.sim_IVP                     import Sim_SolIVP
 from visualizers.vis_PlotTime               import Vis_PlotTime
 from testsuites.tst_Baseline                import Tst_Baseline
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     # instantiate test suite
     tester = Tst_Baseline(sys = Sys_SingleIntegrator,
-                          ctr = Ctr_nCLF,
+                          ctr = Controller_CLF_L2_Origin,
                           sim = Sim_SolIVP,
                           vis = Vis_PlotTime)
 
