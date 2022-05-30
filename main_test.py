0ba1668732c10634ecb1d741106e9eb86f724cca
Alex Toofanian <alextoofanian@gmail.com>
2022-05-30 12:26:01 -0700

    fixed .gitignore

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ import numpy as np
 ###############################################################
 
 from systems.sys_SingleIntegrator           import Sys_SingleIntegrator
-from controllers.ctr_CLF_L2_Origin          import Controller_CLF_L2_Origin
+from controllers.ctr_CLF_L2_origin          import Controller_CLF_L2_Origin
 from simulators.sim_IVP                     import Sim_SolIVP
 from visualizers.vis_PlotTime               import Vis_PlotTime
 from testsuites.tst_Baseline                import Tst_Baseline
