c6d8a5974dde19ff7c7f809cf277fd4a6984e687
Alex Toofanian <alextoofanian@gmail.com>
2022-06-05 21:51:32 -0700

    removed external call to utils for makeproblemclfqp

--- controllers/ctr_nCLF.py
+++ controllers/ctr_nCLF.py
@@ -8,7 +8,6 @@ from types import NoneType
 
 from .ctr_Parents import Controller
 from systems.sys_Parents import ControlAffineSys
-from utilities.clfqp import makeproblem_clfqp
 
 class Ctr_nCLF(Controller):
     '''
@@ -16,12 +15,12 @@ class Ctr_nCLF(Controller):
     '''
     def __init__(self,
                  sys: ControlAffineSys,
-                 model_path:str = 'controllers/trainedNetworks/singleInt_epoch9.pth',
+                 model_path:str = 'controllers/trainedNetworks/InvertedPendulum/epoch100.pth',
                  refcontrol:Optional[Controller]=None) -> None: #TODO implement ref control class
         super().__init__(sys)
         self.nCLF = torch.load(model_path)
         self.refcontrol:Controller = refcontrol(sys) if type(refcontrol) != NoneType else np.zeros((self.sys.uDims,1))
-        self.clfqp = makeproblem_clfqp(self)
+        self.clfqp = self.makeproblem_clfqp()
 
     def u(self,x:ndarray,t:Optional[float]=None) -> ndarray:
         u_ref = self.refcontrol.u(x,t) if type(self.refcontrol) != np.ndarray else self.refcontrol
@@ -44,9 +43,42 @@ class Ctr_nCLF(Controller):
         self.V_param.value = clf_value
         self.LfV_param.value = Lf_V
         self.LgV_param.value = Lg_V
-        self.clfqp.solve()
+        self.clfqp.solve(solver=cp.ECOS)
         
         #assert self.r_var.value < 1e-5, f'r_var value is negative: {self.r_var.value}'
         r_val = self.r_var.value
         u_val = self.u_var.value
         return u_val
+
+
+    def makeproblem_clfqp(self) -> cp.Problem:
+        '''
+        sets up the CLFQP
+        '''
+        ### define variables and parameters
+        self.u_var = cp.Variable((self.sys.uDims,1))
+        self.u_ref_param = cp.Parameter((self.sys.uDims,1))
+        self.r_var = cp.Variable((1,1),nonneg=True)
+        self.r_penalty_param = cp.Parameter((1,1),nonneg=True)
+        self.V_param = cp.Parameter((1,1),nonneg=True) 
+        self.LfV_param =  cp.Parameter((1,1))
+        self.LgV_param = cp.Parameter((1,self.sys.uDims))
+        c = 1 # hyperparameter, should be tied to one used during training
+
+        ### define objective
+        objective_expression = cp.sum_squares(self.u_var - self.u_ref_param) + cp.multiply(self.r_var,self.r_penalty_param)
+        objective = cp.Minimize(objective_expression)
+
+        ### define constraints
+        constraints = []
+        # control constraints
+        for iu in range(self.sys.uDims):
+            constraints.append(self.u_var[iu] >= self.sys.uBounds[iu,0])
+            constraints.append(self.u_var[iu] <= self.sys.uBounds[iu,1])
+        # CLF constraint
+        constraints.append(self.LfV_param + self.LgV_param@self.u_var <= -self.V_param + self.r_var)
+
+        ### assemble problem
+        problem = cp.Problem(objective,constraints)
+        assert problem.is_qp(), 'Problem is not qp'
+        return problem
