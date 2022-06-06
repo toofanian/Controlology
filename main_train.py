e125e62fe1a6445a5367ed37218b5fcf87de5b89
Alex Toofanian <alextoofanian@gmail.com>
2022-06-06 00:20:20 -0700

    minor commenting

--- main_train.py
+++ main_train.py
@@ -1,6 +1,5 @@
 from controllers.trainedNetworks.nnTrainer import train_nCLF
 from systems.sys_SingleIntegrator import Sys_SingleIntegrator
-from systems.sys_InvertedPendulum import Sys_InvertedPendulum
 
 
 if __name__ == '__main__':
@@ -10,6 +9,11 @@ if __name__ == '__main__':
     then, run the trainer.
     '''
 
+    # pick a system
     sys = Sys_SingleIntegrator()
+
+    # initialize the trainer
     trainer = train_nCLF(sys)
+
+    # run the trainer, defining a save path if desired
     trainer.train('controllers/trainedNetworks/SingleIntegrator_test2')
