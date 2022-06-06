0fc7dc02944b9cbd9c739ead93602a38b4722100
Alex Toofanian <alextoofanian@gmail.com>
2022-06-05 21:50:04 -0700

    removed exponential overlay

--- visualizers/vis_PlotTime.py
+++ visualizers/vis_PlotTime.py
@@ -10,12 +10,12 @@ class Vis_PlotTime(Visualizer):
     
     def render(self,x_data:np.ndarray,u_data:np.ndarray):
         def exponential(x0,t):
-            return x0*np.e**(-t)
+            return x0*np.e**(-.5*t)
 
         for i in range(x_data.shape[0]-1):
             plt.plot(x_data[-1,:],x_data[i,:],label=f'x{i+1}')
-            y_exp = exponential(x_data[i,0],x_data[-1,:])
-            plt.plot(x_data[-1,:],y_exp,label='exponential')
+            #y_exp = exponential(x_data[i,0],x_data[-1,:])
+            #plt.plot(x_data[-1,:],y_exp,label='exponential')
 
         
         plt.xlabel('time [sec]')
